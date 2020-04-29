e0aff52ffe707eaf2be57297f6b9c6b087c0448a
James Collins <collijk@uw.edu>
2020-04-29 00:32:18 -0700

    Rollback to pre-public

--- src/covid_model_deaths/__about__.py
+++ src/covid_model_deaths/__about__.py
@@ -6,7 +6,7 @@ __title__ = "covid-model-deaths"
 __summary__ = "IHME Covid-19 Deaths model."
 __uri__ = "https://https://github.com/ihmeuw/covid-model-deaths"
 
-__version__ = '1.0.0'
+__version__ = '0.9.0'
 
 __author__ = "Ryan Barber"
 __email__ = "rmbarber@uw.edu"
