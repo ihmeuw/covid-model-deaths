8d043b0dc8cf48d2f740fb6cc6ad03accf517129
Ryan Barber <rmbarber@uw.edu>
2020-05-18 09:11:06 -0700

    no str.contains

--- notebooks/experiments/alt_deaths_inputs/cdr_model.py
+++ notebooks/experiments/alt_deaths_inputs/cdr_model.py
@@ -168,7 +168,7 @@ def plotter(df: pd.DataFrame, unadj_vars: List[str], adj_vars: List[str],
         #                   np.diff(df[smooth_variable]) * df['population'][1:], 
         #                   **smoothed_lines)
         ax[1, i].axhline(0, color='black', alpha=0.25, linestyle='--')
-        if smooth_variable.contains('death'):
+        if 'death' in smooth_variable:
             ax[1, i].set_xlabel('Date', fontsize=10)
         else:
             ax[1, i].set_xlabel('Date (+8 days)', fontsize=10)
